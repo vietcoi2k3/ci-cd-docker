9ef5a6a2345464741d5702b0e6a8139cd5627185
Viet Do <viet1234561231@gmail.com>
2024-01-20 22:24:25 +0700

    dsad

---
+++ src/main/java/com/example/cicd_gitaction/CicdGitactionApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicd_gitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGitactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdGitactionApplication.class, args);
+		System.out.println("dsadsadsa");
+	}
+
+}
---
+++ src/test/java/com/example/cicd_gitaction/CicdGitactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGitactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
