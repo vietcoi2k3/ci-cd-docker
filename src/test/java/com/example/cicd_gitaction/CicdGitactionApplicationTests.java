aedc072c1aeb5e9f5aa734c325e034cab0205b7c
Viet Do <viet1234561231@gmail.com>
2024-02-03 10:53:21 +0700

    a

--- src/test/java/com/example/cicd_gitaction/CicdGitactionApplicationTests.java
+++ src/test/java/com/example/cicd_gitaction/CicdGitactionApplicationTests.java
@@ -19,7 +19,7 @@ class CicdGitactionApplicationTests {
 		String email = "example@gmail.com";
 		ResponseEntity<String> response = restTemplate.postForEntity("/validateEmail?email=" + email, null, String.class);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertEquals(0,1);
+		assertEquals("Valid", response.getBody());
 	}
 
 	@Test
